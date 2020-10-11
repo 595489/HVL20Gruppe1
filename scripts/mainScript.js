2ddb4d91ca9ae3e592c82827ccf9daa1a1bf13a5
Isak <595489@stud.hvl.no>
2020-10-11 12:56:51 +0200

    JS test 1

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,9 +1,9 @@
-function resizeFiller() {
-	var main = document.getElementsByClassName('mainDiv').offsetHeight;
-	var bodySize = document.getAttribute('body').style.height;
-	var finSize = 1080 - bodySize;
-	if (bodySize < 1080px) {
-		document.getElementsByClassName('mainDiv').style.height = finSize + "px";
-	}
-}
-window.onload = resizeFiller;
+//function resizeFiller() {
+//	var main = document.getElementsByClassName('mainDiv').offsetHeight;
+//	var bodySize = document.getAttribute('body').style.height;
+//	var finSize = 1080 - bodySize;
+//	if (bodySize < 1080px) {
+//		document.getElementsByClassName('mainDiv').style.height = finSize + "px";
+//	}
+//}
+//window.onload = resizeFiller;
