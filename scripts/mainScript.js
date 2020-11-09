3f5b638698c308ab14837c13eb441cffbc24b977
August-berger <594624@stud.hvl.no>
2020-11-09 01:07:08 +0100

    Dropdown
    Dropdown HYML/CSS/JS

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -7,3 +7,23 @@ function resizeFiller() {
 	}
 }
 window.onload = resizeFiller;
+
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+	var dropdowns = document.getElementsByClassName("dropdown-content");
+	var i;
+	for (i = 0; i<dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+		}
+	}
+	}
+}
+
+
